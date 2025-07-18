430cf1c9491a699a4a9bd57ce37b9c423ac4ad34
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-18 09:35:38 +0300

    Test project

---
+++ Draft/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+///  Draft
+/// </summary>
+
+namespace Draft
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<KeyValuePair<char, byte>> pairs = new();
+            Dictionary<char, byte> dic = new(pairs);
+            List<string> names = new();
+            names.AddRange(["Tsvetelin", "Maria", "Antonia"]);
+            bool hasMan = names.Exists(name => name.EndsWith("in"));
+            Console.WriteLine(hasMan);
+            string? mansName = names.Find(name => name.StartsWith("Ts") && name.EndsWith("in"));
+            Console.WriteLine(mansName);
+        }
+    }
+}
